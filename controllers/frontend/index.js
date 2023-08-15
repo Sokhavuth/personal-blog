fd6f0d7db6b98846c901c00b47736c74306028b3
Sokhavuth <vuthdevelop@gmail.com>
2023-08-15 18:27:04 +0700

    Make it better

--- controllers/frontend/index.js
+++ controllers/frontend/index.js
@@ -1,11 +1,15 @@
 //controllers/frontend/index.js
 
+import postDb from "../../models/post.js"
+
 class Index{
     async getPage(req, res){
         req.settings.pageTitle = 'ទំព័រ​ដើម'
         req.settings.message = ''
         req.settings.route = '/'
         req.settings.type = ''
+
+        req.settings.items = await postDb.getPosts(req, req.settings.indexPostLimit)
         
         res.render('base', {data:req.settings})
     }
